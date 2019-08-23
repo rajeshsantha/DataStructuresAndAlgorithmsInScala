bd2e95cf95bc957befce53ca5c6ce80ba322c7fb
RajeshS <rajesh.santha29@gmail.com>
2019-08-24 00:41:40 +0530

    initial commit to remote repo

---
+++ src/com/dsa/java/BubbleSortImpl.java
@@ -0,0 +1,9 @@
+package com.dsa.java;
+
+public class BubbleSortImpl {
+    public static void main(String[] args) {
+        System.out.println("Hello World Java");
+
+    }
+
+}
---
+++ src/com/dsa/scala/BubbleSort.scala
@@ -0,0 +1,8 @@
+package com.dsa.scala
+
+object BubbleSort {
+  def main (args: Array[String]): Unit = {
+    println("Hello World Scala")
+  }
+
+}

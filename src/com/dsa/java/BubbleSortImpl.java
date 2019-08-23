d73d2105baf4e3753244e933c55a5e5367fada9b
RajeshS <rajesh.santha29@gmail.com>
2019-08-24 01:32:40 +0530

    Completed bubble sort in Scala and Java

--- src/com/dsa/java/BubbleSortImpl.java
+++ src/com/dsa/java/BubbleSortImpl.java
@@ -2,8 +2,24 @@ package com.dsa.java;
 
 public class BubbleSortImpl {
     public static void main(String[] args) {
-        System.out.println("Hello World Java");
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            for (int i = 0; i < lastUnsortedIndex; i++) {
+                if (intArray[i] > intArray[i + 1]) {
+                    swap(intArray, i, i + 1);
+                }
+            }
+        }
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
 
+    private static void swap(int[] arr, int i, int j) {
+        if (i == j) return;
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
 }
--- src/com/dsa/scala/BubbleSort.scala
+++ src/com/dsa/scala/BubbleSort.scala
@@ -1,8 +1,28 @@
 package com.dsa.scala
 
 object BubbleSort {
+
   def main (args: Array[String]): Unit = {
-    println("Hello World Scala")
+
+    val intArray = Array(20, 35, -15, 7, 55, 1, -22)
+
+    for (lastUnsortedIndex <- intArray.length - 1 until 0 by -1) {
+
+      for (i <- 0 until lastUnsortedIndex by 1) {
+
+        if (intArray(i) > intArray(i + 1)) {
+          swap(intArray, i, i + 1)
+        }
+      }
+    }
+    for (i <- intArray) println(i)
   }
 
+
+  def swap (arr: Array[Int], i: Int, j: Int) = {
+    if (i == j) {}
+    val temp = arr(i)
+    arr(i) = arr(j)
+    arr(j) = temp
+  }
 }

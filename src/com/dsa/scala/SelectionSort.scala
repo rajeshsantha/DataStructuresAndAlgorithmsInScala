4cdf2f31ef8984bbc38d5d71c139e5911ffe115f
RajeshS <rajesh.santha29@gmail.com>
2019-08-25 15:25:21 +0530

    Completed Selection sort in Scala

---
+++ src/com/dsa/scala/SelectionSort.scala
@@ -0,0 +1,34 @@
+package com.dsa.scala
+
+object SelectionSort {
+
+  def main (args: Array[String]): Unit = {
+
+    val intArray = Array(20, 35, -15, 7, 55, 1, -22)
+    println("**********SelectionSort Scala**********")
+
+    println("Unsorted array")
+    for (i <- intArray) println(i)
+
+    for (lastUnsortedIndex <- intArray.length - 1 until 0 by -1) {
+      var largest = 0
+
+      for (i <- 0 until lastUnsortedIndex by 1) {
+        if (intArray(i) > intArray(largest)) {
+          largest=i
+        }
+      }
+      swap(intArray,largest,lastUnsortedIndex)
+    }
+
+    println(" Sorted array - By SelectionSort ")
+    for (i <- intArray) println(i)
+  }
+
+  def swap (arr: Array[Int], i: Int, j: Int) = {
+    if (i == j) {}
+    val temp = arr(i)
+    arr(i) = arr(j)
+    arr(j) = temp
+  }
+}

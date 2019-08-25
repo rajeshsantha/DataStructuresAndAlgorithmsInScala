b4ce35e18505e4a906e4e0dc8384bf66426e72af
RajeshS <rajesh.santha29@gmail.com>
2019-08-25 15:22:19 +0530

    Completed bubble sort in Java

--- src/com/dsa/java/BubbleSortImpl.java
+++ src/com/dsa/java/BubbleSortImpl.java
@@ -5,9 +5,7 @@ public class BubbleSortImpl {
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
         System.out.println("**********BubbleSort Java**********");
         System.out.println("Unsorted array");
-        for (int i = 0; i < intArray.length; i++) {
-            System.out.println(intArray[i]);
-        }
+        for (int e : intArray) System.out.println(e);
 
         for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
             for (int i = 0; i < lastUnsortedIndex; i++) {
@@ -17,10 +15,7 @@ public class BubbleSortImpl {
             }
         }
         System.out.println("\n Sorted array - By BubbleSort ");
-
-        for (int i = 0; i < intArray.length; i++) {
-            System.out.println(intArray[i]);
-        }
+        for (int e : intArray) System.out.println(e);
 
     }
 

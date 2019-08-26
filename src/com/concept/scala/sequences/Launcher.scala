62055a613c264482637f0e381db6354f76b51928
RajeshS <rajesh.santha29@gmail.com>
2019-08-27 01:04:31 +0530

    completed Fibonacci series functional implementation

--- src/com/concept/scala/sequences/Launcher.scala
+++ src/com/concept/scala/sequences/Launcher.scala
@@ -6,9 +6,14 @@ package com.concept.scala.sequences
   */
 object Launcher {
   def main (args: Array[String]): Unit = {
-    val apGen= new ArithmeticGenerator(0, 5)
+
+    val apGen = new ArithmeticGenerator(0, 5)
     println(apGen.generateStr(10))
-    val gpGen= new GeometricGenerator(1, 5)
+
+    val gpGen = new GeometricGenerator(1, 5)
     println(gpGen.generateStr(10))
+
+    val fbGen= new FibonacciGenerator()
+    println(fbGen.generateStr(10))
   }
 }

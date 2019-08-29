844be32269bfc8c40400d5a8c2255c26e5a4e0cc
rajeshs903 <rajesh.santha29@gmail.com>
2019-08-30 00:13:29 +0530

    shunting yard algorithm To be implemented

---
+++ src/com/concept/scala/ShuntingYardAlgorithm.scala
@@ -0,0 +1,19 @@
+package com.concept.scala
+
+/**
+  * @author Rajesh Santha
+  * @version 1.0 on 29th Aug,2019
+  * @usecase Shunting Yard algorithm implentation to solve infix to postfix convertion AND postfix evaluation
+  * @example
+  * Input string is => 5*(2-3)
+  * expected output => 523-*
+  *
+  */
+
+object ShuntingYardAlgorithm {
+
+  def main(args: Array[String]): Unit = {
+    println("********shunting yard algorithm*********")
+  }
+
+}

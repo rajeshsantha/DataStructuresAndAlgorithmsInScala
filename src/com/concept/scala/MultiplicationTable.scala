de59720b3bc7eab78dc7350f9d90a758948babf2
RajeshS <rajesh.santha29@gmail.com>
2019-09-05 23:24:39 +0530

    Added multiplication table in imperative and functional implementations

---
+++ src/com/concept/scala/MultiplicationTable.scala
@@ -0,0 +1,62 @@
+package com.concept.scala
+
+/*
+
+print multplication table as in below format with proper padding and indentation.
+
+   1  2  3  4  5  6  7  8  9 10
+   2  4  6  8 10 12 14 16 18 20
+   3  6  9 12 15 18 21 24 27 30
+   4  8 12 16 20 24 28 32 36 40
+   5 10 15 20 25 30 35 40 45 50
+   6 12 18 24 30 36 42 48 54 60
+   7 14 21 28 35 42 49 56 63 70
+   8 16 24 32 40 48 56 64 72 80
+   9 18 27 36 45 54 63 72 81 90
+  10 20 30 40 50 60 70 80 90 100
+
+ */
+object MultiplicationTable {
+
+  def main(args: Array[String]): Unit = {
+    multiplication_table_imperative()
+    multiplication_table_functional().foreach(println)
+  }
+
+  /**
+    * Its imperative because its contains vars ,while loops, and having no value be returned
+    */
+  def multiplication_table_imperative() = {
+    println("\nIn multiplication_table_imperative \n")
+    var i = 1
+    while (i <= 10) {
+      var j = 1
+      while (j <= 10) {
+        val multipledResult = (i * j).toString
+        var spaceLength = multipledResult.length
+        while (spaceLength < 4) {
+          print(" ")
+          spaceLength += 1
+        }
+        print(s"$multipledResult")
+        j += 1
+      }
+      println()
+      i += 1
+    }
+  }
+
+  def multiplication_table_functional() = {
+    println("\nIn multiplication_table_functional \n")
+
+    for (i <- 1 to 10) yield {
+      for (j <- 1 to 10) yield {
+        val multipledResult = (i * j).toString
+        val spaceLength = " " * (4 - multipledResult.length)
+        spaceLength + multipledResult
+      }.mkString
+      //println("\n")
+    }.mkString
+
+  }
+}

b535670069149156cda1e8b94d7f9be23e464c4f
rajeshs903 <rajesh.santha29@gmail.com>
2019-09-07 01:13:04 +0530

    started Evaluating e^x

---
+++ src/com/concept/scala/hackerrank/Evaluating_eTox.scala
@@ -0,0 +1,28 @@
+package com.concept.scala.hackerrank
+/**
+  * @author Rajesh Santha
+  * @version 1.0 (No further revison)
+  * @todo
+  * Evaluate {e to the power of x} for given values of  x by using the summation of series for the first 10 terms.
+  *
+  * @Input_Format
+  *
+  * The first line contains an integer N, the number of test cases.
+  * N lines follow. Each line contains a value of x for which you need to output the value of {e to the power of x} using the above series expansion.
+  * These input values have exactly 4 decimal places each.
+  *
+  * @note Use functional programming, not imperative programming
+  *
+  * @see https://www.hackerrank.com/challenges/eval-ex/problem
+  *
+  *
+  */
+
+object Evaluating_eTox {
+
+  def main(args: Array[String]): Unit = {
+
+
+
+  }
+}

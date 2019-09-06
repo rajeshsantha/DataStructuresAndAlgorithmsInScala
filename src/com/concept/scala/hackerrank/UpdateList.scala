64d90c471396579a7a0337d5bb6b5b9b1cd904c3
rajeshs903 <rajesh.santha29@gmail.com>
2019-09-07 01:01:26 +0530

    add UpdateList solution

---
+++ src/com/concept/scala/hackerrank/UpdateList.scala
@@ -0,0 +1,23 @@
+package com.concept.scala.hackerrank
+
+/**
+  * @author Rajesh Santha
+  * @version 1.0 (No further revison)
+  * @todo
+  * Update the values of a list with their absolute values. (do not use ABS)
+  *
+  * @note Use functional programming, avoid imperative programming.
+  *
+  */
+
+object UpdateList {
+
+  def main(args: Array[String]): Unit = {
+
+    //val listArgs = scala.io.Source.stdin.getLines.toList.map(_.trim.toInt) // To run in hackerrank
+    val list1 = List(3, 2, -4, 6, 4, -7, -8, 0, 1) // To run local
+
+    f(list1).foreach(println)
+  }
+  def f(arr:List[Int]):List[Int] = arr.map(x=>if(x<0) -x else x)
+}

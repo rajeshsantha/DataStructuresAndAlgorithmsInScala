79a700403e312c5bc1958907e1b1f4320b7bc8e5
rajeshs903 <rajesh.santha29@gmail.com>
2019-09-08 18:50:29 +0530

    start AreaUnderCurvesAndVolumeRevolvingCurve

---
+++ src/com/concept/scala/hackerrank/AreaUnderCurvesAndVolumeRevolvingCurve.scala
@@ -0,0 +1,18 @@
+package com.concept.scala.hackerrank
+
+/**
+  * @author Rajesh Santha
+  * @version 1.3
+  * @todo refer the below link
+  *
+  * @note Use functional programming, avoid imperative programming (no Vars, no loops, no side effects)
+  *
+  * @see https://www.hackerrank.com/challenges/eval-ex/problem
+  *
+  *
+  */
+
+
+object AreaUnderCurvesAndVolumeRevolvingCurve {
+
+}

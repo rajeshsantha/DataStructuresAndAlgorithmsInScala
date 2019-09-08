c29488626ecd662469728a71f68d1b03015dcf35
rajeshs903 <rajesh.santha29@gmail.com>
2019-09-08 17:24:47 +0530

    add Evaluating_eTox solution

--- src/com/concept/scala/hackerrank/Evaluating_eTox.scala
+++ src/com/concept/scala/hackerrank/Evaluating_eTox.scala
@@ -1,18 +1,18 @@
 package com.concept.scala.hackerrank
+
+import scala.annotation.tailrec
+
 /**
   * @author Rajesh Santha
   * @version 1.0 (No further revison)
   * @todo
   * Evaluate {e to the power of x} for given values of  x by using the summation of series for the first 10 terms.
-  *
   * @Input_Format
   *
   * The first line contains an integer N, the number of test cases.
   * N lines follow. Each line contains a value of x for which you need to output the value of {e to the power of x} using the above series expansion.
   * These input values have exactly 4 decimal places each.
-  *
   * @note Use functional programming, not imperative programming
-  *
   * @see https://www.hackerrank.com/challenges/eval-ex/problem
   *
   *
@@ -23,6 +23,27 @@ object Evaluating_eTox {
   def main(args: Array[String]): Unit = {
 
 
+    val n = scala.io.StdIn.readLine.trim.toInt
 
+    for (nItr <- 1 to n) {
+      val x = scala.io.StdIn.readLine.trim.toDouble
+      println(eToXevaluator(x))
+    }
   }
+
+  def eToXevaluator(num: Double): String = {
+
+    val streamEX: Stream[Double] = Stream.from(0).map(x => math.pow(num, x) / factorial(x))
+    val sum = streamEX.take(10).toList.sum
+    f"$sum%.4f"
+  }
+
+  def factorial(fn: Int): Int = {
+    @tailrec
+    def f(acc: Int, fn: Int): Int = if (fn <= 0) acc else f(acc * fn, fn - 1)
+
+    f(1, fn)
+  }
+
+
 }

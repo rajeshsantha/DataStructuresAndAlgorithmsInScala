cd919015fea63bf3584d932ed12e2ba3d1dd7e9d
Rajesh Santha <rajesh.santha29@gmail.com>
2019-09-18 20:31:42 +0530

    add FunctionsOrNot

---
+++ src/com/concept/scala/hackerrank/functionalProgramming/FunctionsOrNot.scala
@@ -0,0 +1,30 @@
+package com.concept.scala.hackerrank.functionalProgramming
+
+import scala.collection.immutable.HashSet
+import scala.io.StdIn._
+
+/**
+  * @author Rajesh Santha
+  * @version 1.0
+  * @todo identifying a relation which represents a valid function. i.e "Is f(x)=x ?" 
+  * @note Use functional programming, avoid imperative programming (no Vars, no loops, no side effects)
+  * @see https://www.hackerrank.com/challenges/functions-or-not/problem
+  *
+  */
+
+object FunctionsOrNot {
+  def main(args: Array[String]) {
+
+    val T = readInt()
+    (1 to T).map { t =>
+
+      val pairs = (1 to readInt()).map { x =>
+        readLine()
+      }
+      val value = pairs.map(_.split("\\s+")).map(_ (0))
+      val uniq = new HashSet() ++ value
+      if (value.length == uniq.size) "YES" else "NO"
+    }.foreach(println)
+  }
+
+}

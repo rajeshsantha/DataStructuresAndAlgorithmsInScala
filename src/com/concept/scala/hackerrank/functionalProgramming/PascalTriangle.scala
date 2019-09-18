05f95bcc186adbdc95aa0415847834ab4792278e
Rajesh Santha <rajesh.santha29@gmail.com>
2019-09-18 19:28:49 +0530

    code formatted

--- src/com/concept/scala/hackerrank/functionalProgramming/PascalTriangle.scala
+++ src/com/concept/scala/hackerrank/functionalProgramming/PascalTriangle.scala
@@ -9,32 +9,22 @@ import scala.annotation.tailrec
   * @see https://www.hackerrank.com/challenges/pascals-triangle/problem
   *
   */
-
-
 object PascalTriangle {
   def main(args: Array[String]): Unit = {
 
-
     val stdin = scala.io.StdIn
     val number = stdin.readLine.trim.toInt
-
-
     for (n <- 0 until number;
          row <- 0 to n) {
       val num = factorial(n) / (factorial(row) * factorial(n - row))
       print(num + " ")
       if (row == n) println()
     }
-
-
   }
 
   def factorial(fn: Int): Int = {
     @tailrec
     def f(acc: Int, fn: Int): Int = if (fn <= 0) acc else f(acc * fn, fn - 1)
-
     f(1, fn)
   }
-
-
 }

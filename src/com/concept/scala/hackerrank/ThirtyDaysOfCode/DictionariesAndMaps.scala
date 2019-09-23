8d0483155d252da373f9dba54e096e027e10a345
Rajesh Santha <rajesh.santha29@gmail.com>
2019-09-23 13:58:18 +0530

    update DictionariesAndMaps with 10 input testcase

--- src/com/concept/scala/hackerrank/ThirtyDaysOfCode/DictionariesAndMaps.scala
+++ src/com/concept/scala/hackerrank/ThirtyDaysOfCode/DictionariesAndMaps.scala
@@ -4,7 +4,7 @@ import scala.collection.mutable.Map
 import scala.collection.mutable.ListBuffer
 /**
   * @author Rajesh Santha
-  * @version 1.0 (10k input test case version pending) 
+  * @version 1.1 
   * @todo part of 30 days of code challenge. (Arrays)
   * @see https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
   * @note Use functional programming, avoid imperative programming.
@@ -31,6 +31,30 @@ object DictionariesAndMaps extends {
     def getFormattedValue(x: String) = if (totalMap.contains(x)) println(x + "=" + totalMap(x)) else println("Not found")
 
   }
+// Below code for satisfy the testcase 1 with 10k input.
+ import scala.collection.mutable
+     def otherSolution(args: Array[String]) {
+        val sc = new java.util.Scanner(System.in)
+        val n = sc.nextLine.toInt
+        val book = mutable.HashMap[String, String]()
+        for (i <- 0 until n) {
+          val split = sc.nextLine.split(" ")
+          book.put(split(0), split(1))
+        }
+        
+        
+        
+        while (sc.hasNext) {
+            val query = sc.nextLine
+            val nr = book.get(query)
+            if (nr.isDefined) {
+                println(query+"="+nr.get)
+            } else {
+                println("Not found")
+            }
+            //query = sc.nextLine
+        }
+    }
 
 
 }

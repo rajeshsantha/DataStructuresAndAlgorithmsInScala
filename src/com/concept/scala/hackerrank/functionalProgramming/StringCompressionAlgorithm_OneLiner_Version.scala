9a213b281278d42bf9bd5bbea18ff8fce8ff5a32
Rajesh Santha <rajesh.santha29@gmail.com>
2019-10-16 20:31:24 +0530

    Doc commit

--- src/com/concept/scala/hackerrank/functionalProgramming/StringCompressionAlgorithm_OneLiner_Version.scala
+++ src/com/concept/scala/hackerrank/functionalProgramming/StringCompressionAlgorithm_OneLiner_Version.scala
@@ -4,7 +4,7 @@ package com.concept.scala.hackerrank.functionalProgramming
   * @author Rajesh Santha
   * @version 1.0
   * @note Use functional programming, avoid imperative programming.(follwed by Version 1.1 and 1.2)
-  *       This one is a one-linere in functional programming with the help of regex.
+  *       This one is a one-linere in functional programming with the help of regular expression in scala. 
   * @see https://www.hackerrank.com/challenges/string-compression/problem
   * @example If Input string is "abcaaabbb" => output should be abca3b3
   *

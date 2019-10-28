796845ae6aac3110785ee0f660444de244f4a4cd
Rajesh Santha <rajesh.santha29@gmail.com>
2019-10-28 19:31:07 +0530

    removed unused packages

--- src/com/concept/scala/hackerrank/ThirtyDaysOfCode/FactorialSolution.scala
+++ src/com/concept/scala/hackerrank/ThirtyDaysOfCode/FactorialSolution.scala
@@ -1,14 +1,5 @@
 package com.concept.scala.hackerrank.ThirtyDaysOfCode
 
-import java.io._
-import java.math._
-import java.security._
-import java.text._
-import java.util._
-import java.util.concurrent._
-import java.util.function._
-import java.util.regex._
-import java.util.stream._
 import scala.annotation.tailrec
 
 object FactorialSolution {
@@ -25,8 +16,6 @@ object FactorialSolution {
 
     def main(args: Array[String]) {
         val stdin = scala.io.StdIn
-
-        val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
 
         val n = stdin.readLine.trim.toInt
 

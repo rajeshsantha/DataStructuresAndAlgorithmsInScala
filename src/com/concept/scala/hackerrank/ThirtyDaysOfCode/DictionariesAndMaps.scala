e413bbad442e1ed85a72402b5e99e27b7a0464f9
RajeshS <rajesh.santha29@gmail.com>
2019-10-28 19:36:27 +0530

    refactored code functionally

--- src/com/concept/scala/hackerrank/ThirtyDaysOfCode/DictionariesAndMaps.scala
+++ src/com/concept/scala/hackerrank/ThirtyDaysOfCode/DictionariesAndMaps.scala
@@ -1,7 +1,7 @@
 package com.concept.scala.hackerrank.ThirtyDaysOfCode
 
-import scala.collection.mutable.Map
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.{ListBuffer, Map}
+
 /**
   * @author Rajesh Santha
   * @version 1.1 
@@ -11,50 +11,53 @@ import scala.collection.mutable.ListBuffer
   *
   */
 object DictionariesAndMaps extends {
-  def main(args: Array[String]): Unit = {
+  def main (args: Array[String]): Unit = {
 
 
-    var totalList = ListBuffer[String]()
+    val totalList = ListBuffer[String]()
     val totalMap: Map[String, String] = Map()
 
     val stdin = scala.io.StdIn
     val inputLen = stdin.readLine.trim.toInt
-    for (i <- 0 until inputLen) {
+    for (_ <- 0 until inputLen) {
       val rowMap = stdin.readLine.split(" ").grouped(2).map { case Array(k, v) => k -> v }.toMap
       totalMap ++= rowMap
     }
-    for (i <- 0 until inputLen) {
+    for (_ <- 0 until inputLen) {
       val outputReq = stdin.readLine().trim
-      totalList = totalList += outputReq
+      //totalList = totalList += outputReq
+      totalList += outputReq
     }
     totalList.foreach(x => getFormattedValue(x))
-    def getFormattedValue(x: String) = if (totalMap.contains(x)) println(x + "=" + totalMap(x)) else println("Not found")
+
+    def getFormattedValue (x: String) = if (totalMap.contains(x)) println(x + "=" + totalMap(x)) else println("Not found")
 
   }
-// Below code for satisfy the testcase 1 with 10k input.
- import scala.collection.mutable
-     def otherSolution(args: Array[String]) {
-        val sc = new java.util.Scanner(System.in)
-        val n = sc.nextLine.toInt
-        val book = mutable.HashMap[String, String]()
-        for (i <- 0 until n) {
-          val split = sc.nextLine.split(" ")
-          book.put(split(0), split(1))
-        }
-        
-        
-        
-        while (sc.hasNext) {
-            val query = sc.nextLine
-            val nr = book.get(query)
-            if (nr.isDefined) {
-                println(query+"="+nr.get)
-            } else {
-                println("Not found")
-            }
-            //query = sc.nextLine
-        }
+
+  // Below code for satisfy the testcase 1 with 10k input.
+  import scala.collection.mutable
+
+  def otherSolution (args: Array[String]) {
+    val sc = new java.util.Scanner(System.in)
+    val n = sc.nextLine.toInt
+    val book = mutable.HashMap[String, String]()
+    for (i <- 0 until n) {
+      val split = sc.nextLine.split(" ")
+      book.put(split(0), split(1))
     }
+
+
+    while (sc.hasNext) {
+      val query = sc.nextLine
+      val nr = book.get(query)
+      if (nr.isDefined) {
+        println(query + "=" + nr.get)
+      } else {
+        println("Not found")
+      }
+      //query = sc.nextLine
+    }
+  }
 
 
 }

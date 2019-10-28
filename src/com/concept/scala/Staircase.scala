d19e5d2b49bb297f37912652fbcd4f0a5cf62bd9
RajeshS <rajesh.santha29@gmail.com>
2019-10-28 18:43:57 +0530

    add Staircase solution

---
+++ src/com/concept/scala/Staircase.scala
@@ -0,0 +1,34 @@
+package com.concept.scala
+
+/**
+  * @author Rajesh
+  * @see https://www.hackerrank.com/challenges/staircase/problem
+  *
+  */
+object Staircase {
+  def main (args: Array[String]): Unit = {
+
+    staircase(scala.io.StdIn.readInt())
+  }
+
+  def staircase (n: Int): Unit = {
+
+    for (i <- 1 to n by 1) {
+      for (j <- n to 1 by -1)
+        if (i < j) print(" ") else print("#")
+      println("")
+    }
+  }
+
+}
+
+/**
+  * 6 (input)
+  *
+  * #       (output)
+  * ##
+  * ###
+  * ####
+  * #####
+  * ######
+  */

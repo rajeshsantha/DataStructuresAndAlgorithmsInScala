fa6986c4d95c3f186d4376976e6e675baea12a8d
RajeshS <rajesh.santha29@gmail.com>
2019-11-01 18:40:45 +0530

    completed Time Conversion problem with case match

--- src/com/concept/scala/hackerrank/warmups/TimeConversion.scala
+++ src/com/concept/scala/hackerrank/warmups/TimeConversion.scala
@@ -5,23 +5,45 @@ package com.concept.scala.hackerrank.warmups
   */
 object TimeConversion {
   def main (args: Array[String]): Unit = {
-    val inputTime = "12:45:54PM"
+    val inputTime1 = "11:00:54AM"
+    val inputTime2 = "9:45:54PM"
+    val inputTime3 = "12:45:54PM"
+    println(convertTo24HFormat(inputTime1)) //11:00:54
+    println(convertTo24HFormat(inputTime2)) // 21:45:54
+    println(convertTo24HFormat(inputTime3)) // 12:45:54
+    /*
+        val len = inputTime length
+        val meridiem = inputTime substring (len - 2)
+        val timeArray = inputTime slice(0, len - 2) split (":")
+
+        if (meridiem == "PM") {
+          val hour = if (timeArray(0) != "12") timeArray(0).toInt + 12 else timeArray(0).toInt
+
+          println(hour + ":" + timeArray.drop(1).mkString(":"))
+        }
+        if (meridiem == "AM") {
+          if (timeArray(0) == "12") timeArray(0) = "00"
+
+          println(timeArray.mkString(":"))
+        }
+    */
+
+  }
+
+  def convertTo24HFormat (inputTime: String): String = {
     val len = inputTime length
     val meridiem = inputTime substring (len - 2)
     val timeArray = inputTime slice(0, len - 2) split (":")
 
-    if (meridiem == "PM") {
-      val hour = if (timeArray(0) != "12") timeArray(0).toInt + 12 else timeArray(0).toInt
+    val finalTime = meridiem match {
+      case "AM" => if (timeArray(0) == "12") timeArray(0) = "00"; timeArray.mkString(":")
 
-      println(hour + ":" + timeArray.drop(1).mkString(":"))
+
+      case "PM" => {
+        val hour = if (timeArray(0) != "12") timeArray(0).toInt + 12 else timeArray(0).toInt
+        hour + ":" + timeArray.drop(1).mkString(":")
+      }
     }
-    if (meridiem == "AM") {
-      if (timeArray(0) == "12") timeArray(0) = "00"
-
-      println(timeArray.mkString(":"))
-
-    }
-
+    finalTime
   }
-
 }

7d78327f13cf506ce4e0e732479edf0bea3f9bbb
Rajesh Santha <rajesh.santha29@gmail.com>
2019-11-07 20:03:25 +0530

    remove comments

--- src/com/concept/scala/hackerrank/warmups/TimeConversion.scala
+++ src/com/concept/scala/hackerrank/warmups/TimeConversion.scala
@@ -11,22 +11,7 @@ object TimeConversion {
     println(convertTo24HFormat(inputTime1)) //11:00:54
     println(convertTo24HFormat(inputTime2)) // 21:45:54
     println(convertTo24HFormat(inputTime3)) // 12:45:54
-    /*
-        val len = inputTime length
-        val meridiem = inputTime substring (len - 2)
-        val timeArray = inputTime slice(0, len - 2) split (":")
-
-        if (meridiem == "PM") {
-          val hour = if (timeArray(0) != "12") timeArray(0).toInt + 12 else timeArray(0).toInt
-
-          println(hour + ":" + timeArray.drop(1).mkString(":"))
-        }
-        if (meridiem == "AM") {
-          if (timeArray(0) == "12") timeArray(0) = "00"
-
-          println(timeArray.mkString(":"))
-        }
-    */
+   
 
   }
 

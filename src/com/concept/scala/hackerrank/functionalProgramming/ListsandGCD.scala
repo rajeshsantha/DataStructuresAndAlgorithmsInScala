5b653f63082892ef4af1a086c823fee606b16aec
rajeshs903 <rajesh.santha29@gmail.com>
2020-04-30 18:17:54 +0530

    in progress

---
+++ src/com/concept/scala/hackerrank/functionalProgramming/ListsandGCD.scala
@@ -0,0 +1,27 @@
+package com.concept.scala.hackerrank.functionalProgramming
+
+/** *
+  *
+  * @see https://www.hackerrank.com/challenges/lists-and-gcd/problem
+  *
+  */
+object ListsandGCD {
+  def main(args: Array[String]) {
+    /*   var hashmap = HashMap[Int, Int]()
+
+       for (_ <- 1 to readLine.toInt) {
+
+         val t = readLine.split(" ").map(_.toInt).grouped(2).map(x => (x(0), x(1))).toMap
+         if (hashmap.isEmpty)
+           hashmap ++= t
+         else {
+
+           (hashmap.keySet -- t.keySet).foreach(k => hashmap -= k)
+           (hashmap.keySet & t.keySet).foreach(k => hashmap(k) = hashmap(k) min t(k))
+         }
+       }
+       println(hashmap.toList.sorted.map(x => s"${x._1} ${x._2}").mkString(" "))
+   */
+  }
+
+}
---
+++ src/com/concept/scala/hackerrank/functionalProgramming/TheSumsofPowers.scala
@@ -0,0 +1,31 @@
+package com.concept.scala.hackerrank.functionalProgramming
+
+/** *
+  *
+  * @see https://www.hackerrank.com/challenges/functional-programming-the-sums-of-powers/problem
+  *
+  *
+  */
+object TheSumsofPowers {
+  def numberOfWays(X: Int, N: Int): Int = {
+    def calculatePowers(x: Int, n: Int, num: Int): Int = {
+      val numbersTillN = math.pow(num, n).toInt
+
+      if (numbersTillN > x) {
+        return 0
+      } else if (numbersTillN == x) {
+        return 1
+      } else {
+        return calculatePowers(x, n, num + 1) + calculatePowers(x - numbersTillN, n, num + 1)
+      }
+    }
+
+    calculatePowers(X, N, 1)
+  }
+
+  def main(args: Array[String]) {
+
+    println(numberOfWays(scala.io.StdIn.readInt(), scala.io.StdIn.readInt()))
+  }
+
+}

41eb713bc082818b2074752daa753a49d90d739b
RajeshS <rajesh.santha29@gmail.com>
2020-06-18 18:59:31 +0530

    resolve name conflict TreeNode

--- src/com/concept/scala/leetcode_30days_challenge_June2020/SearchInBinarySearchTree.scala
+++ src/com/concept/scala/leetcode_30days_challenge_June2020/SearchInBinarySearchTree.scala
@@ -83,11 +83,11 @@ object SearchInBinarySearchTree {
     }
 
   }
+  class TreeNode (_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+    var value: Int = _value
+    var left: TreeNode = _left
+    var right: TreeNode = _right
+  }
 
 }
 
-class TreeNode (_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
-  var value: Int = _value
-  var left: TreeNode = _left
-  var right: TreeNode = _right
-}

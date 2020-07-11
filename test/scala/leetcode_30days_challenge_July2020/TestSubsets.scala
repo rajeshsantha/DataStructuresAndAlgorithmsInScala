e4e2dc2b9cb3ff4e4cf373ffcc8cdb3ba739f0d4
rajeshsantha <rajesh.santha29@gmail.com>
2020-07-11 23:09:28 +0530

    add TestSubsets unit test for Day 11

---
+++ test/scala/leetcode_30days_challenge_July2020/TestSubsets.scala
@@ -0,0 +1,20 @@
+package scala.leetcode_30days_challenge_July2020
+
+import com.concept.scala.leetcode_30days_challenge_July2020.Subsets.subsets
+import org.scalatest.{FlatSpec, Matchers}
+
+class TestSubsets extends FlatSpec with Matchers {
+
+  trait TestSubsetsList {
+    val testcaseResult1: List[List[Int]] = subsets(Array(1, 2, 3))
+    val testcaseResult2: List[List[Int]] = subsets(Array(1, 2, 3, 3))
+    val testcaseResult3: List[List[Int]] = subsets(Array(1))
+  }
+
+  "result set" should "combinations of all subsets without duplicates" in new TestSubsetsList {
+    testcaseResult1 should be(List(List(), List(1), List(2), List(3), List(1, 2), List(1, 3), List(2, 3), List(1, 2, 3)))
+    testcaseResult2 should not be (List(List(), List(1), List(2), List(3), List(1, 2), List(1, 3), List(2, 3), List(1, 2, 3)))
+    testcaseResult3 should be(List(List(), List(1)))
+  }
+
+}

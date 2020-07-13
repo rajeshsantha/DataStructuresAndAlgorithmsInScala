8a2ba41b385e1f2a9f6a8a05913f8c977937d786
Rajesh Santha <rajesh.santha29@gmail.com>
2020-07-13 21:15:03 +0530

    add SameTrees - Day 13

--- src/com/concept/scala/leetcode_30days_challenge_July2020/SameTrees.scala
+++ src/com/concept/scala/leetcode_30days_challenge_July2020/SameTrees.scala
@@ -14,28 +14,28 @@ import scala.annotation.tailrec
   * @example Example 1:
   *
   *          Input:     1         1
-  *          / \       / \
-  *          2   3     2   3
+  *                    / \       / \
+  *                   2   3     2   3
   *
-  *          [1,2,3],   [1,2,3]
+  *                 [1,2,3],   [1,2,3]
   *
   *          Output: true
   *          Example 2:
   *
   *          Input:     1         1
-  *          /           \
-  *          2             2
+  *                    /           \
+  *                   2             2
   *
-  *          [1,2],     [1,null,2]
+        *          [1,2],     [1,null,2]
   *
   *          Output: false
   *          Example 3:
   *
   *          Input:     1         1
-  *          / \       / \
-  *          2   1     1   2
+  *                    / \       / \
+  *                   2   1     1   2
   *
-  *          [1,2,1],   [1,1,2]
+  *                 [1,2,1],   [1,1,2]
   *
   *          Output: false
   * @see https://leetcode.com/problems/same-tree/

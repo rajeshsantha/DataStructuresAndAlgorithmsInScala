3a6317d6efd70bbe1bccb8000e75886c1e5e4181
Rajesh Santha <rajesh.santha29@gmail.com>
2020-08-07 22:31:41 +0530

    update TestSubsets with comments

--- test/scala/leetcode_30days_challenge_July2020/TestSubsets.scala
+++ test/scala/leetcode_30days_challenge_July2020/TestSubsets.scala
@@ -11,6 +11,7 @@ class TestSubsets extends FlatSpec with Matchers {
     val testcaseResult3: List[List[Int]] = subsets(Array(1))
   }
 
+  //execute the test cases as below
   "result set" should "combinations of all subsets without duplicates" in new TestSubsetsList {
     testcaseResult1 should be(List(List(), List(1), List(2), List(3), List(1, 2), List(1, 3), List(2, 3), List(1, 2, 3)))
     testcaseResult2 should not be (List(List(), List(1), List(2), List(3), List(1, 2), List(1, 3), List(2, 3), List(1, 2, 3)))

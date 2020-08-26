a58ebac9cf70bdea479fe4cd56551827b319d4bb
Rajesh Santha <rajesh.santha29@gmail.com>
2020-08-27 00:15:35 +0530

    add FizzBuzz - Day 26 - update ouput

--- src/com/concept/scala/leetcode_30days_challenge_August2020/FizzBuzz.scala
+++ src/com/concept/scala/leetcode_30days_challenge_August2020/FizzBuzz.scala
@@ -45,6 +45,7 @@ object FizzBuzz {
   def fizzBuzz(n: Int): List[String] = (1 to n).map(logic).map(_.toString).toList
 
   def main(args: Array[String]): Unit = println(fizzBuzz(28).mkString(","))
+//output : 1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz,Fizz,22,23,Fizz,Buzz,26,Fizz,28
 
   def logic(num: Int): Any = num match {
     case x if isMultipleOf15(x) => "FizzBuzz"

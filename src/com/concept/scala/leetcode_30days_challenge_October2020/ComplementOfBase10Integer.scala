c59f32f0a2161899bfbcc129c088f376c8cad9c9
Rajesh Santha <rajesh.santha29@gmail.com>
2020-10-07 21:33:24 +0530

    update ComplementOfBase10Integer - day 5

--- src/com/concept/scala/leetcode_30days_challenge_October2020/ComplementOfBase10Integer.scala
+++ src/com/concept/scala/leetcode_30days_challenge_October2020/ComplementOfBase10Integer.scala
@@ -2,7 +2,7 @@ package com.concept.scala.leetcode_30days_challenge_October2020
 
 /** **
   *
-  * Day 6
+  * Day 5
   *
   * @todo Every non-negative integer N has a binary representation.  For example, 5 can be represented as "101" in binary, 11 as "1011" in binary, and so on.  Note that except for N = 0, there are no leading zeroes in any binary representation.
   *       The complement of a binary representation is the number in binary you get when changing every 1 to a 0 and 0 to a 1.  For example, the complement of "101" in binary is "010" in binary.

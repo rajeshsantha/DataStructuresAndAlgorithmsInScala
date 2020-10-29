c4c1d5a1b17d921088ab432be72447ff2548ab52
Rajesh Santha <rajesh.santha29@gmail.com>
2020-10-29 22:17:18 +0530

    add day 28 doc

--- src/com/concept/scala/leetcode_30days_challenge_October2020/SummaryRanges.scala
+++ src/com/concept/scala/leetcode_30days_challenge_October2020/SummaryRanges.scala
@@ -2,7 +2,7 @@ package com.concept.scala.leetcode_30days_challenge_October2020
 
 
 /** **
-  *
+  * D
   * @todo You are given a sorted unique integer array nums.
   *       Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
   *       Each range [a,b] in the list should be output as:

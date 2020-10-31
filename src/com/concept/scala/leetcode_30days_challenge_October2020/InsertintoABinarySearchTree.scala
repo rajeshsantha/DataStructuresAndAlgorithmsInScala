a7440ede4776265f23da47638989aa5c4ea53a23
Rajesh Santha <rajesh.santha29@gmail.com>
2020-10-31 21:43:07 +0530

    Day 6

--- src/com/concept/scala/leetcode_30days_challenge_October2020/InsertintoABinarySearchTree.scala
+++ src/com/concept/scala/leetcode_30days_challenge_October2020/InsertintoABinarySearchTree.scala
@@ -4,7 +4,7 @@ import com.concept.scala.leetcode_30days_challenge_April2020.TreeNode
 
 /** *
   *
-  * Day 6
+  * day 6
   *
   * @todo You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
   *       Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.

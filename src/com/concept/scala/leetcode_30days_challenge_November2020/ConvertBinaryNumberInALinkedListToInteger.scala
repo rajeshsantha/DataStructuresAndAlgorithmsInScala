9afa74e3466488b33f21124077a4c77af5f0aabd
Rajesh Santha <rajesh.santha29@gmail.com>
2020-11-02 21:21:02 +0530

    Day 1

--- src/com/concept/scala/leetcode_30days_challenge_November2020/ConvertBinaryNumberInALinkedListToInteger.scala
+++ src/com/concept/scala/leetcode_30days_challenge_November2020/ConvertBinaryNumberInALinkedListToInteger.scala
@@ -6,7 +6,7 @@ import scala.collection.mutable
 
 /** **
   *
-  * Day 1
+  * Day 1 - November 
   *
   * @todo Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
   *       Return the decimal value of the number in the linked list.

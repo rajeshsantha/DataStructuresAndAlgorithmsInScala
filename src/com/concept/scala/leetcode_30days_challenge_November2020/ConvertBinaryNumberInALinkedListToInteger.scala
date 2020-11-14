b84229b69ddd1a44331fb21a82dff6b1ddc8934d
Rajesh Santha <rajesh.santha29@gmail.com>
2020-11-14 22:45:53 +0530

    add ConvertBinaryNumberInALinkedListToInteger - day 1

--- src/com/concept/scala/leetcode_30days_challenge_November2020/ConvertBinaryNumberInALinkedListToInteger.scala
+++ src/com/concept/scala/leetcode_30days_challenge_November2020/ConvertBinaryNumberInALinkedListToInteger.scala
@@ -39,7 +39,6 @@ import scala.collection.mutable
   *      OR
   *      https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer
   *
-  *
   */
 object ConvertBinaryNumberInALinkedListToInteger {
 

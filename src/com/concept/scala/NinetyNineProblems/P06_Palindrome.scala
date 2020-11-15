d6ce6109f549b952af2e80e549a7779a08a12ebe
Rajesh Santha <rajesh.santha29@gmail.com>
2020-11-15 23:34:16 +0530

    add P06_Palindrome

---
+++ src/com/concept/scala/NinetyNineProblems/P06_Palindrome.scala
@@ -0,0 +1,8 @@
+package com.concept.scala.NinetyNineProblems
+
+object P06_Palindrome {
+  def isPalindrome_builtin[A](ls: List[A]): Boolean = ls == ls.reverse
+  def isPalindromeFunctional[T](list: List[T]): Boolean = list ./:(List[T]()) { (a, b) => b :: a } == list
+
+
+}

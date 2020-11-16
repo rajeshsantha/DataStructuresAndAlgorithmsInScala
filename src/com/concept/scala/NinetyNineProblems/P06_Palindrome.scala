d82dfb694fb1d1060a5ec67cf7ecea44a74e6bbd
Rajesh Santha <rajesh.santha29@gmail.com>
2020-11-16 19:10:55 +0530

    Update P06_Palindrome.scala

--- src/com/concept/scala/NinetyNineProblems/P06_Palindrome.scala
+++ src/com/concept/scala/NinetyNineProblems/P06_Palindrome.scala
@@ -1,8 +1,5 @@
 package com.concept.scala.NinetyNineProblems
-
 object P06_Palindrome {
   def isPalindrome_builtin[A](ls: List[A]): Boolean = ls == ls.reverse
   def isPalindromeFunctional[T](list: List[T]): Boolean = list ./:(List[T]()) { (a, b) => b :: a } == list
-
-
 }

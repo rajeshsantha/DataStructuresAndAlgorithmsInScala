3d262115ce2deb1ddacf1907db521f4f7abb31fc
Rajesh Santha <rajesh.santha29@gmail.com>
2020-11-17 22:21:46 +0530

    update doc

--- src/com/concept/scala/NinetyNineProblems/ReverseList.scala
+++ src/com/concept/scala/NinetyNineProblems/ReverseList.scala
@@ -8,7 +8,6 @@ import scala.annotation.tailrec
   *       to understand the language concepts clearly
   * @example //     scala> reverse(List(1, 9, 2, 3, 5, 8))
   *          //     res0: List[Int] = List(8, 5, 3, 2, 9, 1)
-  * @groupname Problem - 5
   *
   */
 

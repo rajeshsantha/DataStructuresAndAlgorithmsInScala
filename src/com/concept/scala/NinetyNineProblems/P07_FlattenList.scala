b58f229523a126e73289b2162aafbe2b3d0c8ec9
Rajesh Santha <rajesh.santha29@gmail.com>
2020-11-20 22:37:12 +0530

    add statement

--- src/com/concept/scala/NinetyNineProblems/P07_FlattenList.scala
+++ src/com/concept/scala/NinetyNineProblems/P07_FlattenList.scala
@@ -1,5 +1,9 @@
 package com.concept.scala.NinetyNineProblems
-
+/****
+*
+*@todo P07 (**) Flatten a nested list structure
+*
+***/
 object P07_FlattenList {
   def flatten(list: List[Any]): List[Any] = list flatMap {
     case ms: List[_] => flatten(ms)

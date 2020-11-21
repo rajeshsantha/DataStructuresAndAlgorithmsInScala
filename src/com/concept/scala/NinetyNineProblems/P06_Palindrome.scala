e47fb058505d80c368d8452477a606b77f92595d
Rajesh Santha <rajesh.santha29@gmail.com>
2020-11-21 22:49:18 +0530

    Update P06_Palindrome.scala

--- src/com/concept/scala/NinetyNineProblems/P06_Palindrome.scala
+++ src/com/concept/scala/NinetyNineProblems/P06_Palindrome.scala
@@ -1,4 +1,14 @@
 package com.concept.scala.NinetyNineProblems
+/***
+*// P06 (*) Find out whether a list is a palindrome.
+*//     Example:
+*//     scala> isPalindrome(List(1, 2, 3, 2, 1))
+*//     res0: Boolean = true
+*
+*
+*
+*
+***/
 object P06_Palindrome {
   def isPalindrome_builtin[A](ls: List[A]): Boolean = ls == ls.reverse
   def isPalindromeFunctional[T](list: List[T]): Boolean = list ./:(List[T]()) { (a, b) => b :: a } == list

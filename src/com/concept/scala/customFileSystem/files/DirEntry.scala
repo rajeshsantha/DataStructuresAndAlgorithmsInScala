9c9ad7ba9afe6661977cb90deaf1d811eacd4745
Rajesh <rajesh.santha29@gmail.com>
2020-12-14 00:09:28 +0530

    add custom file system implementations - pending util methods

---
+++ src/com/concept/scala/customFileSystem/files/DirEntry.scala
@@ -0,0 +1,14 @@
+package com.concept.scala.customFileSystem.files
+
+/** *
+  *
+  * could be either Dir or File
+  *
+  * @param parentPath
+  * @param name
+  */
+abstract class DirEntry(val parentPath: String, val name: String) {
+  def path: String = parentPath + Directory.SEPERATOR + name
+
+  def asDirectory: Directory
+}
